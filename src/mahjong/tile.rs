460e62ef7982801d1e29ff05259fd3137c568a23
tamakiii <tamakiii@users.noreply.github.com>
2023-04-25 13:36:29 +0900

    impl tile.rs

--- src/mahjong/tile.rs
+++ src/mahjong/tile.rs
@@ -1,34 +1,24 @@
-pub struct Tile {
-    tile_type: TileType,
-}
-
 pub enum TileType {
     Bamboo(u8),
     Character(u8),
     Circle(u8),
+    Wind(Wind),
+    Dragon(Dragon),
 }
 
-// pub enum TileType {
-//     Bamboo(u8),
-//     Character(u8),
-//     Circle(u8),
-//     Wind(Wind),
-//     Dragon(Dragon),
-// }
+pub enum Wind {
+    East,
+    South,
+    West,
+    North,
+}
 
-// pub enum Wind {
-//     East,
-//     South,
-//     West,
-//     North,
-// }
+pub enum Dragon {
+    Red,
+    Green,
+    White,
+}
 
-// pub enum Dragon {
-//     Red,
-//     Green,
-//     White,
-// }
-
-// pub struct Tile {
-//     tile_type: TileType,
-// }
+pub struct Tile {
+    tile_type: TileType,
+}
