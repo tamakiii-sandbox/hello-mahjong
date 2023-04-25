4593d87ad91bdee2fad23ac014f33af47f3ed851
tamakiii <tamakiii@users.noreply.github.com>
2023-04-25 16:41:20 +0900

    output wall as json

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use mahjong::mahjong::game::initialize;
+use mahjong::mahjong::{game::initialize, view::TileView};
 
 fn main() {
     let game = initialize();
-    println!("Game created!");
+    let view: Vec<TileView> = game.wall.into_iter().map(TileView::from).collect();
+    let json = serde_json::to_string(&view).unwrap();
+    println!("{}", json);
 }
